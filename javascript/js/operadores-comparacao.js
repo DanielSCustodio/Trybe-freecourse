b92b847b3ee23c8c5416a56cf85658eca6b1e387
Daniel <danielcustodio_@outlook.com>
2021-03-26 14:31:40 -0300

    #4 - Operadores de Comparacao - JavaScript

---
+++ javascript/js/operadores-comparacao.js
@@ -0,0 +1,24 @@
+console.log(2>3);
+console.log(3<1);
+console.log(10===10);
+console.log(10!==10);
+
+var nota = 100;
+situacao = nota >= 150;
+console.log(situacao);
+
+if (situacao===true) {
+	console.log("Passou");
+}else{
+	console.log("Nao passou");
+}
+
+
+var notaMaria = 95;
+var notaMinima = 100;
+
+if (notaMaria >= notaMinima) {
+	console.log("You Wins!")
+}else{
+	console.log("You Lost!")
+}
