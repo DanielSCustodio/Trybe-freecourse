8a26295ec4ffd095a8e2aef1846aeb741cc1db4e
Daniel <danielcustodio_@outlook.com>
2021-03-26 17:21:11 -0300

    #7 - Arrays - JavaScript

---
+++ javascript/js/arrays.js
@@ -0,0 +1,24 @@
+var listaNomes = ["Katana", "Mulher-Gavião","Ravena","Ultra-Humanoide","Canário Negro"];
+
+console.log(listaNomes);
+
+console.log(listaNomes[3]);
+
+listaNomes.push("Senhor Frio");
+
+console.log(listaNomes);
+
+var a = listaNomes[listaNomes.length-1];
+console.log(a);
+
+var corBandeira = ["branco","azul","amarelo","cinza","vermelho","preto"];
+ tamanho = corBandeira.length;
+ console.log("Tamanho do array: ",tamanho);
+
+ for (i of corBandeira) {
+ 	console.log(i)
+ }
+
+
+var = "uva"
+var = "abacaxi"
