de329dd703d053f1a2678d48f2f1369f5c0e9798
Daniel <danielcustodio_@outlook.com>
2021-03-26 13:05:46 -0300

    #2 - Tipo Primitivo

--- javascript/js/tipos-primitivos.js
+++ javascript/js/tipos-primitivos.js
@@ -1,2 +1,20 @@
 var nomeEstudante = "Ana";
-console.log(nomeEstudante);
+console.log("nome: ",nomeEstudante);
+
+var aprovacao = true;
+console.log("Situacao: " ,aprovacao);
+
+var hub ="Belo Horizonte";
+console.log("Hub: ",hub);
+
+var turma = 1;
+console.log("Turma: ",turma);
+
+var turmaHub ="Turma 1 - Belo Horizonte";
+console.log("Info: ",turmaHub);
+
+var salario = 3500.70;
+console.log("Salario: ",salario);
+
+
+
