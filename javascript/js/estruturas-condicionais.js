57ff75f6fa156de8623292c48f37e517d55f7e65
Daniel <danielcustodio_@outlook.com>
2021-03-26 16:15:43 -0300

    #6 - Estruturas Condicionais - JavaScript

---
+++ javascript/js/estruturas-condicionais.js
@@ -0,0 +1,30 @@
+var nota = 65;
+var notaMinima = 100;
+
+if (nota >= notaMinima) {
+	console.log("Aprovado")
+} else {
+	console.log("Nao Aprovado")
+}
+
+
+var isUnderstand = true;
+
+if (isUnderstand===true) { console.log("Muito bem! Vamos Prosseguir") } else {
+console.log("Volte ao inicio") }
+
+
+var verde    = false;
+var amarelo  = true;
+
+if (verde) {
+	console.log("SIGA!")
+} else if(amarelo){
+	console.log("ATENCAO!")
+}else{
+	console.log("PARE!")
+}
+var elementoA =20;
+if(elementoA %2 ===0){
+        console.log("ok")
+  }
