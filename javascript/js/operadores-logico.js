ae65d6e29aa7983d5caceaa97158e7d81b4fc3f5
Daniel <danielcustodio_@outlook.com>
2021-03-26 14:54:02 -0300

    #5 - Operadores Logico - JavaScript

---
+++ javascript/js/operadores-logico.js
@@ -0,0 +1,19 @@
+var isWeekend = true;
+var isSun     = true;
+
+var hasMoney    = false;
+var hasGasoline = false;
+
+
+if(isWeekend && isSun){
+	console.log("Partiu praia");
+}else{
+	console.log("Fica de boinha em casa");
+}
+
+
+if (hasMoney || hasGasoline){
+	console.log("Partiu praia");
+}else{
+	console.log("Fica de boinha em casa");
+}
