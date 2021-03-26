9ab58631d1d4dc2715768e92f7926b1b2b479a5f
Daniel <danielcustodio_@outlook.com>
2021-03-26 20:05:45 -0300

    #8 - String - JavaScript

---
+++ javascript/js/strings.js
@@ -0,0 +1,40 @@
+// var name     = "Kent";
+// var lastName = "Cla";
+// var fullName = name +" "+lastName;
+// console.log(fullName);
+// var resultado;
+
+// if(name.length > lastName.length){
+// 	resultado = name+lastName;
+// }else{
+// 	resultado = lastName+name;
+// }
+// console.log(resultado);
+
+// var  mes = "janeiro";
+// var  a   = mes[0];
+// var  b   = mes[1];
+// var  c   = mes[2];
+
+// console.log(a+b+c);
+
+
+
+ var resultado; 
+ var primeiraString = "xx";
+ var segundaString = "tt";
+ 
+
+if(primeiraString.length >= segundaString.length) {
+  resultado = segundaString + primeiraString
+ }else{
+  resultado = primeiraString + segundaString  
+}
+
+if(primeiraString.length >= segundaString.length) {
+  resultado = segundaString + primeiraString
+} else {
+  resultado = primeiraString + segundaString
+}
+
+ console.log(resultado)
